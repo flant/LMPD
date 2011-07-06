01fdd716265076e10eb652295066ec361e5bdfa4
Nikolay Bogdanov <nikolay.bogdanov@trueoffice.ru>
2011-07-06 16:43:16 +0400

    Update copyrights

--- server.py
+++ server.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
-
-#Server.py for postfix policy daemon
+# -*- coding: utf-8 -*-
+#
+#		Main process(connection manager) for policyd
+#       server.py
+#       
+#       Copyright (C) 2009-2011 CJSC TrueOffice (www.trueoffice.ru)
+#		Written by Nikolay aka GyRT Bogdanov <nikolay.bogdanov@trueoffice.ru>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
 
 import os, sys, signal, site
 
