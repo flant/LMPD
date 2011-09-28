602c340bf247d2010701d58ad5453f68bc7bdb3a
Nikolay Bogdanov <nikolay.bogdanov@trueoffice.ru>
2011-09-28 14:13:52 +0400

    Update debianize code

--- tfpolicyd/deb-gen.sh
+++ tfpolicyd/deb-gen.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+CHECKPKG=`dpkg --get-selections | egrep '^(gpgv|lsb-release)' | awk '{print $2}' | grep -v "deinstall" | wc -l`
+if [ ! $CHECKPKG == "2" ]; then
 sudo apt-get -y install lsb-release gpgv
+else
+echo "All needed packages installed"
+fi
 
 BUILDPATH=`pwd`
 
 CODENAME=`lsb_release -a 2>/dev/null |grep "Codename:" | awk '{print $2}'`
 
-sed -i "s/changeme/$CODENAME/g" $BUILDPATH/debian/changelog
+#sed -i "s/changeme/$CODENAME/g" $BUILDPATH/debian/changelog
 
-dpkg-buildpackage -k860D255B
+#dpkg-buildpackage -k860D255B
