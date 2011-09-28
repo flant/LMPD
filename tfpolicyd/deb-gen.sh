8a7266d53beeb714bb88d7a8d3a0297813cac828
Nikolay Bogdanov <nikolay.bogdanov@trueoffice.ru>
2011-09-28 13:17:12 +0400

    Update debianize code

--- tfpolicyd/deb-gen.sh
+++ tfpolicyd/deb-gen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo apt-get -y install lsb-release gpg
+sudo apt-get -y install lsb-release gpgv
 
 BUILDPATH=`pwd`
 
 CODENAME=`lsb_release -a 2>/dev/null |grep "Codename:" | awk '{print $2}'`
 
-sed -i "s/changeme/$K/g" $BUILDPATH/debian/changelog
+sed -i "s/changeme/$CODENAME/g" $BUILDPATH/debian/changelog
 
 dpkg-buildpackage -k09B3B374
